c2c392bba19ad460a31344aaf1aae783dd7b4a43
Tim <theloeschzwerg@web.de>
2022-08-06 18:13:53 +0200

    Create words_on_stream_aid.py

---
+++ words_on_stream_aid.py
@@ -0,0 +1,38 @@
+from sys import argv
+
+from itertools import permutations
+from string import ascii_lowercase
+from nltk.corpus import words, wordnet
+# nltk.download('words')
+# nltk.download('wordnet')
+
+if argv[1]:
+  word = argv[1]
+else:
+  print("Give input, LUL")
+print("##### NEW WORD", "".join(sorted(word)), "#####")
+
+words_ = set(words.words())
+
+likeliness = {
+  0: {4: [], 5: [], 6: [], 7:[], 8: [], 9:[]},
+  1: {4: [], 5: [], 6: [], 7:[], 8: [], 9:[]},
+  2: {4: [], 5: [], 6: [], 7:[], 8: [], 9:[]}
+}
+
+def print_correct(w, syn, certainty):
+  if syn:
+    print(" "*(2-certainty), "!"*certainty, w, "=", syn[0].definition())
+
+for cnt in range(4, len(word)+1):
+  print(f"--- {cnt} letters ---")
+  perms = {"".join(x) for x in set(permutations(word, cnt))}
+  for w in sorted(perms):
+    if '?' in w:
+      for c in ascii_lowercase:
+        rep = w.replace("?",c )
+        syn = wordnet.synsets(rep)
+        print_correct(rep, syn, 0)
+    else:
+      syn = wordnet.synsets(w)
+      print_correct(w, syn, 2 if w in words_ else 1)
